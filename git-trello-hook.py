2efde25b503c0265ac34088ccdb8f3be6e05cc60
Stéfanie Loiseleur <sloiseleur@financeactive.com>
2016-01-28 15:03:22 +0100

    Remove list dependency, add board

--- git-trello-hook.py
+++ git-trello-hook.py
@@ -2,16 +2,14 @@
 
 from gevent import monkey;monkey.patch_all()
 from bottle import route, request,run, default_app
-from trello import Cards, Lists
+from trello import Cards, Boards
 import re
 import json
 
 TRELLO_CONFIG = {
     'api_key': 'cff1385e29ea0ddff0a36d0faef6a1fe',
     'oauth_token': '1607a16ab4ed2f51751179bd5b249e59ccd361e38a662656ccef0c5b3870f5fa',
-    'board_id': 'ZcIBTf5i',
-    'list_id_in_progress': '56a9d3ac7a688d41c24cbd38',
-    'list_id_done': '56a9d3b0c0f71152ab6c651e',
+    'board_id': 'ZcIBTf5i'
 }
 
 WEBHOOK_CONFIG = {
@@ -19,6 +17,7 @@ WEBHOOK_CONFIG = {
     'port': 7343
 }
 
+TRELLO_BOARD = Boards(TRELLO_CONFIG['api_key'], TRELLO_CONFIG['oauth_token'])
 TRELLO_CARDS = Cards(TRELLO_CONFIG['api_key'], TRELLO_CONFIG['oauth_token'])
 
 
@@ -30,10 +29,8 @@ def index():
 @route("/webhook", method='POST')
 def handle_payload():
     json_payload = None
-    from_gitlab = False
     if request.get_header('Content-Type', None) == 'application/json':
         json_payload = request.json
-        from_gitlab = True
     else:
         body = request.forms['payload']
         json_payload = json.loads(body)
@@ -43,7 +40,6 @@ def handle_payload():
     cards_commit_dict = {}
     card_pattern = '(\[)(#)([0-9]+)(\])'
 
-    print(commits)
     for commit in commits:
         results = re.findall(
             card_pattern, commit['message'], flags=re.IGNORECASE)
@@ -56,16 +52,16 @@ def handle_payload():
 
     print(cards_in_commit)
     print(cards_commit_dict)
+    if cards_in_commit:
+        from_cards = TRELLO_BOARD.get_card(TRELLO_CONFIG['board_id'])
 
-    for card_id in cards_in_commit:
-        card = TRELLO_CARDS.get(card_id)
-        if card:
-            print(card)
-            commitForCard = cards_commit_dict[str(card['idShort'])]
-            for acommit in commitForCard:
-                author = acommit['author']['name'].encode('utf-8')
-                desc_with_commit = 'Commit by {0}\n{1}\n{2}'.format(author, acommit['message'].encode('utf-8'), acommit['url'])
-                TRELLO_CARDS.new_action_comment(card['id'], desc_with_commit)
+        for card in from_cards:
+            if str(card['idShort']) in cards_in_commit:
+                commitForCard = cards_commit_dict[str(card['idShort'])]
+                for acommit in commitForCard:
+                    author = acommit['author']['name'].encode('utf-8')
+                    desc_with_commit = 'Commit by {0}\n{1}\n{2}'.format(author, acommit['message'].encode('utf-8'), acommit['url'])
+                    TRELLO_CARDS.new_action_comment(card['id'], desc_with_commit)
 
     return "done"
 
